233264bdc230334551d5f9d9cd055254f2f84a27
Raikem <fulgen_3_7ad@hotmail.com>
2018-05-13 23:30:27 +0200

    Updated README with controls, deleted string in jsonentities, changed webui fov to match spectator fov

--- WebUI/script/webgl.js
+++ WebUI/script/webgl.js
@@ -11,7 +11,7 @@ function init() {
 	renderer.setSize( window.innerWidth, window.innerHeight );
 	document.body.appendChild( renderer.domElement );
 	
-	camera = new THREE.PerspectiveCamera( 74, window.innerWidth / window.innerHeight, 0.01, 3000 );
+	camera = new THREE.PerspectiveCamera( 55, window.innerWidth / window.innerHeight, 0.01, 3000 );
 	camera.position.set( 538, 120, 330 );
 	
 	scene = new THREE.Scene();
