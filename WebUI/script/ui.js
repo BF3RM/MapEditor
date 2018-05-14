c34b14e9057c824cbf1139fda9fb130c4be3f8a5
Mats Bakken <matsabcdefg@hotmail.com>
2018-05-14 13:03:36 +0200

    Added toolbar Moved libs into /scripts/libs Note: Toolbar is not hooked up to any logic.

---
+++ WebUI/script/ui.js
@@ -0,0 +1,15 @@
+$( "#menubar" ).menu({
+	position: {at: "left bottom"},
+	items: "> :not(.ui-widget-seperator)"
+});
+
+$('#worldView').selectmenu();
+
+$('#tools input').checkboxradio({
+	 icon: false
+});
+
+$('#worldSpace input').checkboxradio({
+	 icon: false
+});
+
