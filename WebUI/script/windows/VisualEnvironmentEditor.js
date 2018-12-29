d8ceb30734dc0dca4fd6df596ffd336f0e87d3c9
malcolmhoward <malcolm.g.howard@gmail.com>
2018-12-28 21:21:51 -0500

    Updated categorySelect to display controls for selected category.

--- WebUI/script/windows/VisualEnvironmentEditor.js
+++ WebUI/script/windows/VisualEnvironmentEditor.js
@@ -1,3 +1,24 @@
+// String.format()
+String.prototype.format = function()
+{
+    /*
+    String.format() https://en.wikipedia.org/wiki/String_interpolation
+
+    returns a templated string in which the placeholders are replaced with their corresponding values
+    Example
+    -----------
+    "{0} {1}".format("Hello", "world");
+    Hello world
+    */
+   var content = this;
+   for (var i=0; i < arguments.length; i++)
+   {
+        var replacement = '{' + i + '}';
+        content = content.replace(replacement, arguments[i]);
+   }
+   return content;
+};
+
 class VisualEnvironmentEditor {
 	constructor() {
 		this.dom = null;
@@ -29,35 +50,85 @@ class VisualEnvironmentEditor {
 			"value": "category"
 		});
 
-		let categoryOptions = new Array(19);
-		categoryOptions[0] = 'Info'
-		categoryOptions[1] = 'Presets'
+		function getControlGroupContent(category){
+		    /*
+		    This function returns the content for the selected category option
+		    */
+
+		    let controlGroupContent = "TODO: Display category control group for the {0} category";
+		    let controlGroupMap = new Map([
+		        ["Info", "CinematicTools Yo"]
+		    ]);
+
+		    if (controlGroupMap.has(category)) {
+		        controlGroupContent = controlGroupMap.get(category);
+		    }
+		    else {
+		        if (category === null) {
+                    category = "selected";
+                }
+                controlGroupContent = controlGroupContent.format(category);
+		    }
+
+		    return controlGroupContent;
+		}
+		let categoryOptions = ["Info", "Presets"];
+		let supportedCategories = [
+            "CameraParams",
+            "CharacterLighting",
+            "ColorCorrection",
+            "DamageEffect",
+            "Dof",
+            "DynamicAO",
+            "DynamicEnvmap",
+            "Enlighten",
+            "FilmGrain",
+            "Fog",
+            "LensScope",
+            "MotionBlur",
+            "OutdoorLight",
+            "PlanarReflection",
+            "ScreenEffect",
+            "Sky",
+            "SunFlare",
+            "Tonemap",
+            "Vignette",
+            "Wind"
+		];
+
+        // optionLabel will be the category name passed to getControlGroupMap()
+        let optionLabel = null;
+        // Append supportedCategories to categoryOptions
+		categoryOptions.push.apply(categoryOptions, supportedCategories);
 		for(let i = 0; i < categoryOptions.length; i++){
 		    let option = document.createElement("option");
 
-            option.value = (i+1).toString();
-            let option_label = categoryOptions[i];
-            if (option_label == null){
-                option_label = "sample" + option.value;
+            optionLabel = categoryOptions[i];
+            option.value = optionLabel;
+            if (optionLabel == null){
+                option.value = (i+1).toString();
+                optionLabel = "sample" + option.value;
 		    }
-            option.innerHTML = option_label
-            categorySelect.append(option)
+            option.innerHTML = optionLabel;
+            categorySelect.append(option);
         }
         content.append(categoryControl);
 
         let categoryControlGroup = $(document.createElement("div"));
         categoryControlGroup.addClass("category-control-group");
         let categoryControlGroupText = $(document.createElement("span"));
-        categoryControlGroupText.text("TODO: Display category control group for the selected category");
+        categoryControlGroupText.text(getControlGroupContent("Info"));
         categoryControlGroupText.addClass("category-control-group");
 		categoryControlGroup.append(categoryControlGroupText);
 		content.append(categoryControlGroup);
 
 		this.category = categorySelect;
 
-		$(categorySelect).on('change',function(){
+		$(categorySelect).on('change',function(event){
+            // $this is the categorySelect element that fired the change event
+            let $this = this;
+            categoryControlGroupText.text(getControlGroupContent($this.value));
 			if (editor.selectionGroup.children.length === 0){ return;}
-
 			editor.execute(new SetObjectNameCommand(editor.selectionGroup.children[0].guid, this.value));
 		});
 
