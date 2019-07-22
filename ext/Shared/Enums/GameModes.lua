bf51f6d1129f150edd161a2075cbc6f40143913e
3ti65 <st3ti65@gmail.com>
2019-07-23 00:14:50 +0200

    Added Maps and GameMode enums

---
+++ ext/Shared/Enums/GameModes.lua
@@ -0,0 +1,18 @@
+GameModes = {
+    ConquestLarge0 = { GameModeName = "ConquestLarge0", DisplayName = "Conquest Large" } ,
+    ConquestSmall0 = { GameModeName = "ConquestSmall0", DisplayName = "Conquest Small" } ,
+    ConquestAssaultLarge0 = { GameModeName = "ConquestAssaultLarge0", DisplayName = "Assault64" } ,
+    ConquestAssaultSmall0 = { GameModeName = "ConquestAssaultSmall0", DisplayName = "Assault" } ,
+    ConquestAssaultSmall1 = { GameModeName = "ConquestAssaultSmall1", DisplayName = "Assault #2" } ,
+    RushLarge0 = { GameModeName = "RushLarge0", DisplayName = "Rush" } ,
+    SquadDeathMatch0 = { GameModeName = "SquadDeathMatch0", DisplayName = "Squad Deathmatch" } ,
+    SquadRush0 = { GameModeName = "SquadRush0", DisplayName = "Squad Rush" } ,
+    TeamDeathMatch0 = { GameModeName = "TeamDeathMatch0", DisplayName = "TDM" } ,
+    TeamDeathMatchC0 = { GameModeName = "TeamDeathMatchC0", DisplayName = "TDM Close Quarters" } ,
+    Domination0 = { GameModeName = "Domination0", DisplayName = "Conquest Domination" } ,
+    GunMaster0 = { GameModeName = "GunMaster0", DisplayName = "Gun Master" } ,
+    Scavenger0 = { GameModeName = "Scavenger0", DisplayName = "Scavenger" } ,
+    AirSuperiority0 = { GameModeName = "AirSuperiority0", DisplayName = "Air Superiority" } ,
+    TankSuperiority0 = { GameModeName = "TankSuperiority0", DisplayName = "Tank Superiority" } ,
+    CaptureTheFlag0 = { GameModeName = "CaptureTheFlag0", DisplayName = "CTF" }
+}
---
+++ ext/Shared/Enums/Maps.lua
@@ -0,0 +1,427 @@
+Maps = {
+    MP_001 = {
+        MapName = "MP_001",
+        DisplayName = "Grand Bazaar",
+        GameModes = {
+            GameModes.ConquestLarge0,
+            GameModes.ConquestSmall0,
+            GameModes.RushLarge0,
+            GameModes.SquadDeathMatch0,
+            GameModes.SquadRush0,
+            GameModes.TeamDeathMatch0,
+            GameModes.TeamDeathMatchC0
+        }
+    },
+
+    MP_003 = {
+        MapName = "MP_003",
+        DisplayName = "Teheran Highway",
+        GameModes = {
+            GameModes.ConquestLarge0,
+            GameModes.ConquestSmall0,
+            GameModes.RushLarge0,
+            GameModes.SquadDeathMatch0,
+            GameModes.SquadRush0,
+            GameModes.TeamDeathMatch0,
+            GameModes.TeamDeathMatchC0
+        }
+    },
+
+    MP_007 = {
+        MapName = "MP_007",
+        DisplayName = "Caspian Border",
+        GameModes = {
+            GameModes.ConquestLarge0,
+            GameModes.ConquestSmall0,
+            GameModes.RushLarge0,
+            GameModes.SquadDeathMatch0,
+            GameModes.SquadRush0,
+            GameModes.TeamDeathMatch0,
+            GameModes.TeamDeathMatchC0
+        }
+    },
+
+    MP_011 = {
+        MapName = "MP_011",
+        DisplayName = "Seine Crossing",
+        GameModes = {
+            GameModes.ConquestLarge0,
+            GameModes.ConquestSmall0,
+            GameModes.RushLarge0,
+            GameModes.SquadDeathMatch0,
+            GameModes.SquadRush0,
+            GameModes.TeamDeathMatch0,
+            GameModes.TeamDeathMatchC0
+        }
+    },
+
+    MP_012 = {
+        MapName = "MP_012",
+        DisplayName = "Operation Firestorm",
+        GameModes = {
+            GameModes.ConquestLarge0,
+            GameModes.ConquestSmall0,
+            GameModes.RushLarge0,
+            GameModes.SquadDeathMatch0,
+            GameModes.SquadRush0,
+            GameModes.TeamDeathMatch0,
+            GameModes.TeamDeathMatchC0
+        }
+    },
+
+    MP_013 = {
+        MapName = "MP_013",
+        DisplayName = "Damavand Peak",
+        GameModes = {
+            GameModes.ConquestLarge0,
+            GameModes.ConquestSmall0,
+            GameModes.RushLarge0,
+            GameModes.SquadDeathMatch0,
+            GameModes.SquadRush0,
+            GameModes.TeamDeathMatch0,
+            GameModes.TeamDeathMatchC0
+        }
+    },
+
+    MP_017 = {
+        MapName = "MP_017",
+        DisplayName = "Noshahr Canals",
+        GameModes = {
+            GameModes.ConquestLarge0,
+            GameModes.ConquestSmall0,
+            GameModes.RushLarge0,
+            GameModes.SquadDeathMatch0,
+            GameModes.SquadRush0,
+            GameModes.TeamDeathMatch0,
+            GameModes.TeamDeathMatchC0
+        }
+    },
+
+    MP_018 = {
+        MapName = "MP_018",
+        DisplayName = "Kharg Island",
+        GameModes = {
+            GameModes.ConquestLarge0,
+            GameModes.ConquestSmall0,
+            GameModes.RushLarge0,
+            GameModes.SquadDeathMatch0,
+            GameModes.SquadRush0,
+            GameModes.TeamDeathMatch0,
+            GameModes.TeamDeathMatchC0
+        }
+    },
+
+    MP_Subway = {
+        MapName = "MP_Subway",
+        DisplayName = "Operation Metro",
+        GameModes = {
+            GameModes.ConquestLarge0,
+            GameModes.ConquestSmall0,
+            GameModes.RushLarge0,
+            GameModes.SquadDeathMatch0,
+            GameModes.SquadRush0,
+            GameModes.TeamDeathMatch0,
+            GameModes.TeamDeathMatchC0
+        }
+    },
+
+    XP1_001 = {
+        MapName = "XP1_001",
+        DisplayName = "Strike at Karkand",
+        GameModes = {
+            GameModes.ConquestAssaultLarge0,
+            GameModes.ConquestAssaultSmall0,
+            GameModes.ConquestAssaultSmall1,
+            GameModes.RushLarge0,
+            GameModes.SquadDeathMatch0,
+            GameModes.SquadRush0,
+            GameModes.TeamDeathMatch0,
+            GameModes.TeamDeathMatchC0
+        }
+    },
+
+    XP1_002 = {
+        MapName = "XP1_002",
+        DisplayName = "Gulf of Oman",
+        GameModes = {
+            GameModes.ConquestAssaultSmall0,
+            GameModes.ConquestLarge0,
+            GameModes.ConquestSmall0,
+            GameModes.RushLarge0,
+            GameModes.SquadDeathMatch0,
+            GameModes.SquadRush0,
+            GameModes.TeamDeathMatch0,
+            GameModes.TeamDeathMatchC0
+        }
+    },
+
+    XP1_003 = {
+        MapName = "XP1_003",
+        DisplayName = "Sharqi Peninsula",
+        GameModes = {
+            GameModes.ConquestAssaultLarge0,
+            GameModes.ConquestAssaultSmall0,
+            GameModes.ConquestAssaultSmall1,
+            GameModes.RushLarge0,
+            GameModes.SquadDeathMatch0,
+            GameModes.SquadRush0,
+            GameModes.TeamDeathMatch0,
+            GameModes.TeamDeathMatchC0
+        }
+    },
+
+    XP1_004 = {
+        MapName = "XP1_004",
+        DisplayName = "Wake Island",
+        GameModes = {
+            GameModes.ConquestAssaultLarge0,
+            GameModes.ConquestAssaultSmall0,
+            GameModes.ConquestAssaultSmall1,
+            GameModes.RushLarge0,
+            GameModes.SquadDeathMatch0,
+            GameModes.SquadRush0,
+            GameModes.TeamDeathMatch0,
+            GameModes.TeamDeathMatchC0
+        }
+    },
+
+    XP2_Factory = {
+        MapName = "XP2_Factory",
+        DisplayName = "Scrapmetal",
+        GameModes = {
+            GameModes.Domination0,
+            GameModes.GunMaster0,
+            GameModes.SquadDeathMatch0,
+            GameModes.TeamDeathMatchC0
+        }
+    },
+
+    XP2_Office = {
+        MapName = "XP2_Office",
+        DisplayName = "Operation 925",
+        GameModes = {
+            GameModes.Domination0,
+            GameModes.GunMaster0,
+            GameModes.SquadDeathMatch0,
+            GameModes.TeamDeathMatchC0
+        }
+    },
+
+    XP2_Palace = {
+        MapName = "XP2_Palace",
+        DisplayName = "Donya Fortress",
+        GameModes = {
+            GameModes.Domination0,
+            GameModes.GunMaster0,
+            GameModes.SquadDeathMatch0,
+            GameModes.TeamDeathMatchC0
+        }
+    },
+
+    XP2_Skybar = {
+        MapName = "XP2_Skybar",
+        DisplayName = "Ziba Tower",
+        GameModes = {
+            GameModes.Domination0,
+            GameModes.GunMaster0,
+            GameModes.SquadDeathMatch0,
+            GameModes.TeamDeathMatchC0
+        }
+    },
+
+    XP3_Alborz = {
+        MapName = "XP3_Alborz",
+        DisplayName = "Alborz Mountains",
+        GameModes = {
+            GameModes.ConquestLarge0,
+            GameModes.ConquestSmall0,
+            GameModes.RushLarge0,
+            GameModes.SquadDeathMatch0,
+            GameModes.SquadRush0,
+            GameModes.TankSuperiority0,
+            GameModes.TeamDeathMatch0,
+            GameModes.TeamDeathMatchC0
+        }
+    },
+
+    XP3_Desert = {
+        MapName = "XP3_Desert",
+        DisplayName = "Bandar Desert",
+        GameModes = {
+            GameModes.ConquestLarge0,
+            GameModes.ConquestSmall0,
+            GameModes.RushLarge0,
+            GameModes.SquadDeathMatch0,
+            GameModes.SquadRush0,
+            GameModes.TankSuperiority0,
+            GameModes.TeamDeathMatch0,
+            GameModes.TeamDeathMatchC0
+        }
+    },
+
+    XP3_Shield = {
+        MapName = "XP3_Shield",
+        DisplayName = "Armored Shield",
+        GameModes = {
+            GameModes.ConquestLarge0,
+            GameModes.ConquestSmall0,
+            GameModes.RushLarge0,
+            GameModes.SquadDeathMatch0,
+            GameModes.SquadRush0,
+            GameModes.TankSuperiority0,
+            GameModes.TeamDeathMatch0,
+            GameModes.TeamDeathMatchC0
+        }
+    },
+
+    XP3_Valley = {
+        MapName = "XP3_Valley",
+        DisplayName = "Death Valley",
+        GameModes = {
+            GameModes.ConquestLarge0,
+            GameModes.ConquestSmall0,
+            GameModes.RushLarge0,
+            GameModes.SquadDeathMatch0,
+            GameModes.SquadRush0,
+            GameModes.TankSuperiority0,
+            GameModes.TeamDeathMatch0,
+            GameModes.TeamDeathMatchC0
+        }
+    },
+
+    XP4_FD = {
+        MapName = "XP4_FD",
+        DisplayName = "Markaz Monolith",
+        GameModes = {
+            GameModes.ConquestLarge0,
+            GameModes.ConquestSmall0,
+            GameModes.GunMaster0,
+            GameModes.RushLarge0,
+            GameModes.Scavenger0,
+            GameModes.SquadDeathMatch0,
+            GameModes.SquadRush0,
+            GameModes.TeamDeathMatch0,
+            GameModes.TeamDeathMatchC0
+        }
+    },
+
+    XP4_Parl = {
+        MapName = "XP4_Parl",
+        DisplayName = "Azadi Palace",
+        GameModes = {
+            GameModes.ConquestLarge0,
+            GameModes.ConquestSmall0,
+            GameModes.GunMaster0,
+            GameModes.RushLarge0,
+            GameModes.Scavenger0,
+            GameModes.SquadDeathMatch0,
+            GameModes.SquadRush0,
+            GameModes.TeamDeathMatch0,
+            GameModes.TeamDeathMatchC0
+        }
+    },
+
+    XP4_Quake = {
+        MapName = "XP4_Quake",
+        DisplayName = "Epicenter",
+        GameModes = {
+            GameModes.ConquestLarge0,
+            GameModes.ConquestSmall0,
+            GameModes.GunMaster0,
+            GameModes.RushLarge0,
+            GameModes.Scavenger0,
+            GameModes.SquadDeathMatch0,
+            GameModes.SquadRush0,
+            GameModes.TeamDeathMatch0,
+            GameModes.TeamDeathMatchC0
+        }
+    },
+    
+    XP4_Rubble = {
+        MapName = "XP4_Rubble",
+        DisplayName = "Talah Market",
+        GameModes = {
+            GameModes.ConquestAssaultLarge0,
+            GameModes.ConquestAssaultSmall0,
+            GameModes.GunMaster0,
+            GameModes.RushLarge0,
+            GameModes.Scavenger0,
+            GameModes.SquadDeathMatch0,
+            GameModes.SquadRush0,
+            GameModes.TeamDeathMatch0,
+            GameModes.TeamDeathMatchC0
+        }
+    },
+    
+    XP5_001 = {
+        MapName = "XP5_001",
+        DisplayName = "Operation Riverside",
+        GameModes = {
+            GameModes.AirSuperiority0,
+            GameModes.CaptureTheFlag0,
+            GameModes.ConquestAssaultLarge0,
+            GameModes.ConquestAssaultSmall0,
+            GameModes.ConquestLarge0,
+            GameModes.ConquestSmall0,
+            GameModes.RushLarge0,
+            GameModes.SquadDeathMatch0,
+            GameModes.SquadRush0,
+            GameModes.TeamDeathMatch0,
+            GameModes.TeamDeathMatchC0
+        }
+    },
+    
+    XP5_002 = {
+        MapName = "XP5_002",
+        DisplayName = "Nebandan Flats",
+        GameModes = {
+            GameModes.AirSuperiority0,
+            GameModes.CaptureTheFlag0,
+            GameModes.ConquestAssaultLarge0,
+            GameModes.ConquestAssaultSmall0,
+            GameModes.ConquestLarge0,
+            GameModes.ConquestSmall0,
+            GameModes.RushLarge0,
+            GameModes.SquadDeathMatch0,
+            GameModes.SquadRush0,
+            GameModes.TeamDeathMatch0,
+            GameModes.TeamDeathMatchC0
+        }
+    },
+    
+    XP5_003 = {
+        MapName = "XP5_003",
+        DisplayName = "Kiasar Railroad",
+        GameModes = {
+            GameModes.AirSuperiority0,
+            GameModes.CaptureTheFlag0,
+            GameModes.ConquestAssaultLarge0,
+            GameModes.ConquestAssaultSmall0,
+            GameModes.ConquestLarge0,
+            GameModes.ConquestSmall0,
+            GameModes.RushLarge0,
+            GameModes.SquadDeathMatch0,
+            GameModes.SquadRush0,
+            GameModes.TeamDeathMatch0,
+            GameModes.TeamDeathMatchC0
+        }
+    },
+
+    XP5_004 = {
+        MapName = "XP5_004",
+        DisplayName = "Sabalan Pipeline",
+        GameModes = {
+            GameModes.AirSuperiority0,
+            GameModes.CaptureTheFlag0,
+            GameModes.ConquestAssaultLarge0,
+            GameModes.ConquestAssaultSmall0,
+            GameModes.ConquestLarge0,
+            GameModes.ConquestSmall0,
+            GameModes.RushLarge0,
+            GameModes.SquadDeathMatch0,
+            GameModes.SquadRush0,
+            GameModes.TeamDeathMatch0,
+            GameModes.TeamDeathMatchC0
+        }
+    }
+}
