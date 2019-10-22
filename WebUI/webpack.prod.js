e53835a8d70a545dfc1110e34466aec0efde8dfd
paulhobbel <itsdizzy40@gmail.com>
2019-10-23 01:01:45 +0200

    Minify js/css in production mode

--- WebUI/webpack.prod.js
+++ WebUI/webpack.prod.js
@@ -1,9 +1,22 @@
 const webpackConfig = require('./webpack.base');
 
 webpackConfig
-    .mode('production')
-    .devtool('source-map')
+  .mode('production')
+  .devtool('source-map')
 
-// TODO: Minify js/css
+webpackConfig
+  .plugin('define')
+  .tap(() => [{
+    'process.env': {
+        'NODE_ENV': 'production'
+    }
+  }]);
+
+webpackConfig.optimization
+  .minimizer('terser')
+    .use(require('terser-webpack-plugin'), [{}])
+    .end()
+  .minimizer('css')
+    .use(require('optimize-css-assets-webpack-plugin'), [{}])
 
 module.exports = webpackConfig.toConfig();
