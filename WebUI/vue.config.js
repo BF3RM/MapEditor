ec49c12c219064bf9786705a56e7d3b668d6751e
Mats Bakken <powback@gmail.com>
2020-04-02 21:10:16 +0200

    Remove google-fonts-webpack-plugin

--- WebUI/vue.config.js
+++ WebUI/vue.config.js
@@ -1,15 +1,8 @@
 const { VextPackPlugin } = require('vextpack');
 
-const GoogleFontsPlugin = require('@beyonk/google-fonts-webpack-plugin');
 module.exports = {
 	configureWebpack: {
 		plugins: [
-			new GoogleFontsPlugin({
-				fonts: [
-					{ family: 'Overpass Mono' },
-					{ family: 'Roboto' }
-				]
-			}),
 			new VextPackPlugin({
 				// OPTIONAL: Specify the location of the vuic compiler (not included in this package!)
 				/// defaults to process.env.VUICC_PATH
