473f2beb9c8c545af811250572c0f58a18dd7a1a
Mats Bakken <powback@gmail.com>
2020-08-28 16:38:01 +0200

    Fixed timestamp in load menu

--- WebUI/src/script/components/WindowComponents/ProjectSettingsComponent.vue
+++ WebUI/src/script/components/WindowComponents/ProjectSettingsComponent.vue
@@ -127,11 +127,17 @@ export default class ProjectSettingsComponent extends Vue {
 			unixTimestamp = Date.now() - unixTimestamp;
 		}
 		const date = new Date(unixTimestamp);
-		const hours = date.getHours() - 1;
+		// Hours part from the timestamp
+		const hours = date.getHours();
+		// Minutes part from the timestamp
 		const minutes = '0' + date.getMinutes();
+		// Seconds part from the timestamp
 		const seconds = '0' + date.getSeconds();
 
-		return hours + 'h, ' + minutes.substr(-2) + 'm, ' + seconds.substr(-2) + 's';
+		// Will display time in 10:30:23 format
+		var formattedTime = hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
+
+		return date.toDateString() + ' - ' + formattedTime;
 	}
 }
 </script>
