d93c27b804ed8aeebeea5b22654b2893fdfe09e0
Mats Bakken <powback@gmail.com>
2021-02-01 18:40:12 +0100

    fix ebx old value being new value

--- WebUI/src/script/commands/SetEBXFieldCommand.ts
+++ WebUI/src/script/commands/SetEBXFieldCommand.ts
@@ -3,6 +3,7 @@ import { VextCommand } from '@/script/types/VextCommand';
 import Command from '@/script/libs/three/Command';
 import { CtrRef } from '@/script/types/CtrRef';
 import { Guid } from '@/script/types/Guid';
+import { isPrintable } from '@/script/modules/Utils';
 
 export interface IEBXFieldData {
 	guid?: Guid,
@@ -13,9 +14,11 @@ export interface IEBXFieldData {
 	oldValue?: any | undefined,
 }
 export default class SetEBXFieldCommand extends Command {
+	private oldValue: any = null;
 	constructor(public EBXFieldUpdateData: IEBXFieldData) {
 		super('SetEBXFieldCommand');
 		this.name = 'Change EBX Data';
+		this.getOldValue(EBXFieldUpdateData);
 	}
 
 	public execute() {
@@ -36,11 +39,20 @@ export default class SetEBXFieldCommand extends Command {
 			guid: this.EBXFieldUpdateData.guid,
 			overrides: [{
 				field: this.EBXFieldUpdateData.field,
-				value: this.EBXFieldUpdateData.oldValue,
+				value: this.oldValue,
 				type: this.EBXFieldUpdateData.type,
 				reference: this.EBXFieldUpdateData.reference
 			}]
 		});
 		window.vext.SendCommand(new VextCommand(this.type, gameObjectTransferData));
+	}
+
+	private getOldValue(updateData: IEBXFieldData) {
+		this.oldValue = JSON.parse(JSON.stringify(updateData));
+		let data = this.oldValue;
+		while (!isPrintable(data.type)) {
+			data = data.value;
+		}
+		data.value = data.oldValue;
 	}
 }
