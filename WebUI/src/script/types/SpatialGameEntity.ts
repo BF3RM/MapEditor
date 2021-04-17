23e7a2bcf4eb4828859f829ebabc958d1946206c
FoolHen <fulgen_3_7ad@hotmail.com>
2021-04-17 19:42:52 +0200

    fix: fix web compile error
    (cherry picked from commit 2757c800251896d80fd138f4e0890622a5bc0f0d)

--- WebUI/src/script/types/SpatialGameEntity.ts
+++ WebUI/src/script/types/SpatialGameEntity.ts
@@ -1,29 +1,28 @@
-import * as THREE from 'three';
 import { LinearTransform } from '@/script/types/primitives/LinearTransform';
 import { AxisAlignedBoundingBox } from '@/script/types/AxisAlignedBoundingBox';
 import { IGameEntity } from '@/script/interfaces/IGameEntity';
-import { MeshBasicMaterial, Vector3 } from 'three';
+import { Box3, BoxGeometry, BufferAttribute, BufferGeometry, Color, Mesh, MeshBasicMaterial, Vector3 } from 'three';
 import { Vec3 } from '@/script/types/primitives/Vec3';
 import { RAYCAST_LAYER } from '@/script/types/Enums';
-import Instance from '@/script/types/ebx/Instance';
 import { CtrRef } from '@/script/types/CtrRef';
+import { Geometry } from 'three/examples/jsm/deprecated/Geometry';
 
-export class SpatialGameEntity extends THREE.Mesh implements IGameEntity {
-	private static SELECTED_COLOR: THREE.Color = new THREE.Color(0xFF0000);
-	private static HIGHLIGHTED_COLOR: THREE.Color = new THREE.Color(0x999999);
+export class SpatialGameEntity extends Mesh implements IGameEntity {
+	private static SELECTED_COLOR: Color = new Color(0xFF0000);
+	private static HIGHLIGHTED_COLOR: Color = new Color(0x999999);
 	private instanceId: number;
 	public transform: LinearTransform;
-	private box: THREE.Box3;
+	private box: Box3;
 	private initiatorRef: CtrRef;
 
 	constructor(instanceId: number, transform: LinearTransform, aabb: AxisAlignedBoundingBox, initiatorRef: CtrRef) {
-		const pointsGeom = new THREE.Geometry();
+		const pointsGeom = new Geometry();
 		pointsGeom.vertices.push(
 			aabb.min,
 			aabb.max
 		);
 
-		const center = new THREE.Vector3().copy(pointsGeom.vertices[0]).add(pointsGeom.vertices[1]).multiplyScalar(0.5);
+		const center = new Vector3().copy(pointsGeom.vertices[0]).add(pointsGeom.vertices[1]).multiplyScalar(0.5);
 
 		let vmax = aabb.max;
 		if (vmax.x > 100000 || vmax.y > 100000 || vmax.z > 10000) {
@@ -33,14 +32,14 @@ export class SpatialGameEntity extends THREE.Mesh implements IGameEntity {
 		if (vmin.x > 100000 || vmin.y > 100000 || vmin.z > 10000) {
 			vmin = new Vec3(0, 0, 0);
 		}
-		const boxGeom = new THREE.BoxGeometry(
+		const boxGeom = new BoxGeometry(
 			vmax.x - vmin.x,
 			vmax.y - vmin.y,
 			vmax.z - vmin.z
 		);
 		boxGeom.translate(center.x, center.y, center.z);
 
-		super(boxGeom, new THREE.MeshBasicMaterial({
+		super(boxGeom, new MeshBasicMaterial({
 			color: SpatialGameEntity.SELECTED_COLOR,
 			wireframe: true,
 			visible: true
@@ -50,16 +49,16 @@ export class SpatialGameEntity extends THREE.Mesh implements IGameEntity {
 		const indices = new Uint16Array([0, 1, 1, 2, 2, 3, 3, 0, 4, 5, 5, 6, 6, 7, 7, 4, 0, 4, 1, 5, 2, 6, 3, 7]);
 		const positions = new Float32Array(8 * 3);
 
-		const geometry = new THREE.BufferGeometry();
-		geometry.setIndex(new THREE.BufferAttribute(indices, 1));
-		geometry.setAttribute('position', new THREE.BufferAttribute(positions, 3));
+		const geometry = new BufferGeometry();
+		geometry.setIndex(new BufferAttribute(indices, 1));
+		geometry.setAttribute('position', new BufferAttribute(positions, 3));
 
 		this.instanceId = instanceId;
 		this.transform = transform;
 		this.matrixAutoUpdate = false;
 
 		this.visible = false;
-		this.box = new THREE.Box3(
+		this.box = new Box3(
 			aabb.min,
 			aabb.max);
 		this.updateMatrix();
