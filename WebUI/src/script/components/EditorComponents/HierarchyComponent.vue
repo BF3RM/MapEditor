bc6a1795df7dc0c3d2d4be01813f3c82a6bc1afc
FoolHen <fulgen_3_7ad@hotmail.com>
2021-07-03 17:39:26 +0200

    fix: fix hierachy and explorer row heigh

--- WebUI/src/script/components/EditorComponents/HierarchyComponent.vue
+++ WebUI/src/script/components/EditorComponents/HierarchyComponent.vue
@@ -9,6 +9,7 @@
 									:autoOpen="true"
 									:data="data"
 									:selectable="true"
+									:row-height="13"
 									:should-select-node="shouldSelectNode">
 				<expandable-tree-slot slot-scope="{ node, index, tree, active }"
 									:has-visibility-options="true"
