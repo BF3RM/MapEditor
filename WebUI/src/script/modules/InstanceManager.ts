c944cb14eb86eb278ac7b7b274feea1466abf7a5
FoolHen <fulgen_3_7ad@hotmail.com>
2021-07-08 12:35:34 +0200

    feature(instancing): scale invisible objects to 0

--- WebUI/src/script/modules/InstanceManager.ts
+++ WebUI/src/script/modules/InstanceManager.ts
@@ -1,10 +1,9 @@
 import { SpatialGameEntity } from '@/script/types/SpatialGameEntity';
-import { signals } from '@/script/modules/Signals';
-import { BoxGeometry, Color, DynamicDrawUsage, InstancedMesh, Matrix4, MeshBasicMaterial } from 'three';
+import { BoxGeometry, Color, DynamicDrawUsage, InstancedMesh, Matrix4, MeshBasicMaterial, Vector3 } from 'three';
 
 export default class InstanceManager {
 	private static instance: InstanceManager;
-
+	private readonly count = 1000;
 	private instanceId = 0;
 	private boxGeom = new BoxGeometry(
 		1,
@@ -12,18 +11,26 @@ export default class InstanceManager {
 		1
 	);
 
+	private scale0Matrix = new Matrix4().scale(new Vector3(0, 0, 0));
+
 	private material = new MeshBasicMaterial({
 		color: new Color(SpatialGameEntity.HIGHLIGHTED_COLOR),
 		wireframe: true,
 		visible: true
 	});
 
-	private instancedMesh = new InstancedMesh(this.boxGeom, this.material, 100000);
+	private instancedMesh = new InstancedMesh(this.boxGeom, this.material, this.count);
 
 	private constructor() {
 		this.boxGeom.translate(0, 0.5, 0);
 		this.instancedMesh.instanceMatrix.setUsage(DynamicDrawUsage);
 		editor.threeManager.scene.add(this.instancedMesh);
+
+		// Set all objects as invisible (scaled by 0) to start with
+		for (let i = 0; i < this.count; i++) {
+			this.instancedMesh.setMatrixAt(i, this.scale0Matrix);
+		}
+		this.instancedMesh.instanceMatrix.needsUpdate = true;
 	}
 
 	public static getInstance(): InstanceManager {
@@ -54,13 +61,14 @@ export default class InstanceManager {
 		editor.threeManager.setPendingRender();
 	}
 
+	// All objects in the instancedMesh group are always visible, so in order to hide them we need to scale them to 0
 	SetVisibility(entity: SpatialGameEntity, visible: boolean) {
 		if (visible) {
 			this.SetMatrixFromSpatialEntity(entity);
 			return;
 		}
 
-		this.instancedMesh.setMatrixAt(entity.instanceId, new Matrix4());
+		this.instancedMesh.setMatrixAt(entity.instanceId, this.scale0Matrix);
 		this.instancedMesh.instanceMatrix.needsUpdate = true;
 		editor.threeManager.setPendingRender();
 	}
